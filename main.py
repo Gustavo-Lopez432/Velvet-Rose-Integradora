f4c829138e17bb2a967dc9eee2ca36dabdbe13f4
GusDev <yael.gustavolop@gmail.com>
2026-08-03 01:35:57 -0600

    agregamos datos a las tablas de ventas, productos y empleados

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import flet as ft
+from UI.dashboard_window import dashboard
+from UI.productos_window import productos_window
 from UI.ventas_window import ventas_window
+from UI.empleados_window import empleados_window
 
-ft.app (target=ventas_window)
+ft.app (target=dashboard)
